e3ca93c695e00aea0bfa3395fdeee51ad29a09de
Jason Cavanaugh <jascav418@gmail.com>
2022-11-02 16:57:24 -0400

    Kitchen contents list

--- js/contents.js
+++ js/contents.js
@@ -1,37 +1,52 @@
-const form = document.querySelector("#form");
-const input = document.querySelector("#input");
-const elements = document.querySelector("#items");
-let itemList = [];
+//toggle add item modal
+function toggleAddItemModal() {
+  const modal = document.getElementsByClassName("add-item-modal")[0];
+  if (modal.classList.contains("active")) {
+    modal.classList.remove("active");
+  } else {
+    modal.classList.add("active");
+  }
+}
+
+const form = document.getElementsByClassName("new-item-form")[0];
 
 //add item
-form.onsubmit = async (event) => {
-    event.preventDefault();
-    const item = input.value;
-    await itemList.push(item);
-    await getItems();
-    form.reset();
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const item = input.value;
+  itemList.push(item);
+  getItems();
+  form.reset();
 };
 
+
+const getFoodItems(label, )
 // display items
-const getItems = async () => {
-    elements.innerHTML = await itemList
-        .map(
-            (item, index) => `
-    
+const getItems = () => {
+  getExpiring();
+  getDairy();
+  getFruits();
+  getProtein();
+  getVeggies();
+  elements.innerHTML = itemList
+    .map(
+      (item, index) => `
         <div class="item">
-        <div class="content">
-        <input id="edit" class="text" readonly="readonly" type="text" value= ${item}>
+          <div class="content">
+          <input id="edit" class="text" readonly="readonly" type="text" value= ${item}>
+          </div>
+          <div class="actions">
+          <button class="delete" onclick="deleteItem(event, ${index})">Delete</button>
+          </div>
         </div>
-        <div class="actions">
-        <button class="delete" onclick="deleteItem(event, ${index})">Delete</button>
-        </div>
-        </div>
-        `).join("");
+        `
+    )
+    .join("");
 };
 window.onload = getItems;
 
 // delete item
-const deleteItem = async (event, index) => {
-    await itemList.splice(index, 1);
-    await getItems();
+const deleteItem = (event, index) => {
+  itemList.splice(index, 1);
+  getItems();
 };
