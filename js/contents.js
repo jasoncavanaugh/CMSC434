79d3a076db8dff61fed2d6b99a7624c48eb73a33
Jason Cavanaugh <jascav418@gmail.com>
2022-11-03 12:04:19 -0400

    Added items based on categories

--- js/contents.js
+++ js/contents.js
@@ -8,44 +8,77 @@ function toggleAddItemModal() {
   }
 }
 
-const form = document.getElementsByClassName("new-item-form")[0];
+const newItemForm = document.getElementsByClassName("new-item-form")[0];
+
+const expiring = [];
+const dairy = [];
+const fruits = [];
+const protein = [];
+const veggies = [];
+const grains = [];
 
 //add item
-form.onsubmit = (event) => {
+newItemForm.onsubmit = (event) => {
   event.preventDefault();
-  const item = input.value;
-  itemList.push(item);
-  getItems();
-  form.reset();
+  const name = document.getElementById("new-item-name").value;
+  const type = document.getElementById("food-groups-dropdown").value;
+  console.log(type);
+  let itemList = [];
+  //this could be nicer
+  switch (type) {
+    case "dairy":
+      itemList = dairy;
+      break;
+    case "fruits":
+      itemList = fruits;
+      break;
+    case "grains":
+      itemList = grains;
+      break;
+    case "protein":
+      itemList = protein;
+      break;
+    case "veggies":
+      itemList = veggies;
+      break;
+  }
+
+  const expiration = document.getElementById("expiration-date").value;
+
+  itemList.push({ name, expiration });
+  console.log(itemList);
+  displayFoodItems(`${type}-lst`, itemList);
+  newItemForm.reset();
+  toggleAddItemModal();
 };
 
+const displayFoodItems = (lstId, itemList) => {
+  const lst = document.getElementById(lstId);
 
-const getFoodItems(label, )
-// display items
-const getItems = () => {
-  getExpiring();
-  getDairy();
-  getFruits();
-  getProtein();
-  getVeggies();
-  elements.innerHTML = itemList
-    .map(
-      (item, index) => `
-        <div class="item">
-          <div class="content">
-          <input id="edit" class="text" readonly="readonly" type="text" value= ${item}>
-          </div>
-          <div class="actions">
-          <button class="delete" onclick="deleteItem(event, ${index})">Delete</button>
-          </div>
-        </div>
-        `
-    )
+  lst.innerHTML = itemList
+    .map((item, idx) => {
+      return `
+      <li class="food-item">
+        <h3>${item.name}</h3>
+        <span>Expires: ${item.expiration}</span>
+      </li>
+      `;
+    })
     .join("");
 };
-window.onload = getItems;
 
-// delete item
+// display items
+const displayItems = () => {
+  displayFoodItems("expiring-lst", expiring);
+  displayFoodItems("dairy-lst", dairy);
+  displayFoodItems("fruits-lst", fruits);
+  displayFoodItems("grains-lst", grains);
+  displayFoodItems("protein-lst", protein);
+  displayFoodItems("veggies-lst", veggies);
+};
+window.onload = displayItems;
+
+// delete item. Have to add this back in.
 const deleteItem = (event, index) => {
   itemList.splice(index, 1);
   getItems();
