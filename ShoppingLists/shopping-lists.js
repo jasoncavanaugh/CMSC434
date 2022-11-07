884e7b8c1914e2111e360b008a7518d668e8fd31
Jason Cavanaugh <jascav418@gmail.com>
2022-11-07 17:04:04 -0500

    Amoni integration stuff

--- ShoppingLists/shopping-lists.js
+++ ShoppingLists/shopping-lists.js
@@ -3,10 +3,10 @@ window.onload = main;
 function main() {
   deleteItem();
   clickItem();
-  document.getElementById("create").onclick = create;
-  document.getElementById("add-list").onclick = storeValue;
-  document.getElementById("add-button").onclick = add;
-  document.getElementById("add-item").onclick = addItem;
+  // document.getElementById("create").onclick = create;
+  // document.getElementById("add-list").onclick = storeValue;
+  // document.getElementById("add-button").onclick = add;
+  // document.getElementById("add-item").onclick = addItem;
 }
 
 function deleteItem() {
@@ -27,7 +27,11 @@ function clickItem() {
       function (entry) {
         if (entry.target.tagName === "LI") {
           // if a list item
-          entry.target.classList.add("checked"); // add class of checked
+          if (entry.target.classList.contains("checked")) {
+            entry.target.classList.remove("checked"); // add class of checked
+          } else {
+            entry.target.classList.add("checked"); // add class of checked
+          }
         }
       },
       false
@@ -35,56 +39,68 @@ function clickItem() {
   });
 }
 
-function create() {
-  alert('Under development: Create list button will not work after pressing close');
-  document.getElementById("add-list-form").style.display = "block";
-}
-
-function add() {
-  document.getElementById("add-item-form").style.display = "block";
-}
-
-function addItem() {
-  var newListItem = document.createElement("li");
-
-  var foodName = document.getElementById("food-name").value;
-  var foodGroup = document.getElementById("food-group").value;
-
-  newListItem.append(foodName);
-  if (foodGroup === "veggies") {
-    document.getElementById("veggies").appendChild(newListItem);
-  } else if (foodGroup === "dairy") {
-    document.getElementById("dairy").appendChild(newListItem);
-  } else if (foodGroup === "fruits") {
-    document.getElementById("fruits").appendChild(newListItem);
-  } else if (foodGroup === "proteins") {
-    document.getElementById("proteins").appendChild(newListItem);
-  } else if (foodGroup === "grains") {
-    document.getElementById("grains").appendChild(newListItem);
+function toggleCreateListModal() {
+  const modal = document.getElementById("add-item-modal");
+  console.log(modal);
+  if (modal.classList.contains("active")) {
+    modal.classList.remove("active");
   } else {
-    document.getElementById("drinks").appendChild(newListItem);
-  }
-
-  document.getElementById("food-name").value = "";
-  document.getElementById("food-group").value = "";
-  document.getElementById("add-item-form").style.display = "none";
-
-  var span = document.createElement("span");
-  span.className = "close";
-  newListItem.appendChild(span);
-
-  var deleteItem = document.getElementsByClassName("close");
-  for (item = 0; item < deleteItem.length; item++) {
-    deleteItem[item].addEventListener("click", function () {
-      // every item with close class
-      this.parentElement.style.display = "none"; // change display to none
-    });
+    modal.classList.add("active");
   }
 }
 
-function storeValue() {
-  var listName = document.getElementById("list-name").value;
-  localStorage.setItem("listValue", listName);
-  document.getElementById("list-name").value = "";
-  window.location.href = "newlist.html";
-}
+// function createList() {
+//   alert(
+//     "Under development: Create list button will not work after pressing close"
+//   );
+//   document.getElementById("add-list-form").style.display = "block";
+// }
+
+// function add() {
+//   document.getElementById("add-item-form").style.display = "block";
+// }
+
+// function addItem() {
+//   var newListItem = document.createElement("li");
+
+//   var foodName = document.getElementById("food-name").value;
+//   var foodGroup = document.getElementById("food-group").value;
+
+//   newListItem.append(foodName);
+//   if (foodGroup === "veggies") {
+//     document.getElementById("veggies").appendChild(newListItem);
+//   } else if (foodGroup === "dairy") {
+//     document.getElementById("dairy").appendChild(newListItem);
+//   } else if (foodGroup === "fruits") {
+//     document.getElementById("fruits").appendChild(newListItem);
+//   } else if (foodGroup === "proteins") {
+//     document.getElementById("proteins").appendChild(newListItem);
+//   } else if (foodGroup === "grains") {
+//     document.getElementById("grains").appendChild(newListItem);
+//   } else {
+//     document.getElementById("drinks").appendChild(newListItem);
+//   }
+
+//   document.getElementById("food-name").value = "";
+//   document.getElementById("food-group").value = "";
+//   document.getElementById("add-item-form").style.display = "none";
+
+//   var span = document.createElement("span");
+//   span.className = "close";
+//   newListItem.appendChild(span);
+
+//   var deleteItem = document.getElementsByClassName("close");
+//   for (item = 0; item < deleteItem.length; item++) {
+//     deleteItem[item].addEventListener("click", function () {
+//       // every item with close class
+//       this.parentElement.style.display = "none"; // change display to none
+//     });
+//   }
+// }
+
+// function storeValue() {
+//   var listName = document.getElementById("list-name").value;
+//   localStorage.setItem("listValue", listName);
+//   document.getElementById("list-name").value = "";
+//   window.location.href = "newlist.html";
+// }
